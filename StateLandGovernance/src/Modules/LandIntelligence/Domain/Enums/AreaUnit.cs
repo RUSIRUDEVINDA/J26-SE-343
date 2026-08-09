66c5e416557f86bdf86ab6362f4052dac568eed1
ravindu <ravindu.peiris11@gmail.com>
2026-08-09 12:13:26 +0530

    Domain Layer implementation 01

---
+++ StateLandGovernance/src/Modules/LandIntelligence/Domain/Enums/AreaUnit.cs
@@ -0,0 +1,8 @@
+namespace StateLandGovernance.LandIntelligence.Domain.Enums;
+
+public enum AreaUnit
+{
+    SquareMeters = 1,
+    Hectares = 2,
+    Acres = 3
+}
---
+++ StateLandGovernance/src/Modules/LandIntelligence/Domain/Enums/LandCategoryType.cs
@@ -0,0 +1,9 @@
+namespace StateLandGovernance.LandIntelligence.Domain.Enums;
+
+public enum LandCategoryType
+{
+    StateLand = 1,
+    CrownLand = 2,
+    ReservedLand = 3,
+    Other = 99
+}
