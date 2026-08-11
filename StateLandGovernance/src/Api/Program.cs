fe52d59c73a0f643e5088f5c7bfc3d677334b34c
OvindiVimasha <it23293526@my.sliit.lk>
2026-08-11 12:32:00 +0530

    feat: register WorkflowGovernance module

--- StateLandGovernance/src/Api/Program.cs
+++ StateLandGovernance/src/Api/Program.cs
@@ -1,10 +1,23 @@
+using StateLandGovernance.BuildingBlocks.DependencyInjection;
+using StateLandGovernance.WorkflowGovernance.Application;
+using StateLandGovernance.WorkflowGovernance.Infrastructure;
+using StateLandGovernance.WorkflowGovernance.Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // TODO: Register shared infrastructure services (logging, persistence, security, storage)
 // TODO: Register BuildingBlocks (CQRS, events, observability)
+builder.Services.AddBuildingBlocks();
+
 // TODO: Register LandIntelligence module (Application + Infrastructure + Presentation)
 // TODO: Register LeaseFeasibility module (Application + Infrastructure + Presentation)
 // TODO: Register WorkflowGovernance module (Application + Infrastructure + Presentation)
+
+builder.Services
+    .AddWorkflowGovernanceApplication()
+    .AddWorkflowGovernanceInfrastructure()
+    .AddWorkflowGovernancePresentation();
+
 // TODO: Register GovernanceIntelligence module (Application + Infrastructure + Presentation)
 
 var app = builder.Build();
